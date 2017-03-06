1c89e9275e010581ce9a8c42c1fccf6cbbc28b1f
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-03-06 09:03:45 -0500

    Fix variable name conflict

--- algorithms/quick.js
+++ algorithms/quick.js
@@ -1,30 +1,47 @@
-const reverseOrder = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ];
-const y_end = 600;
-const n = reverseOrder.length;
+let quickArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ];
+const quick_y_end = 300;
+let quickArrays_to_render = [];
+let quickLength = quickArray.length - 1;
 
-function quickSort(stage){
-
+function quickSort(stage3){
+  quickSortByIndex(0, quickLength);
 }
 
+function quickSortByIndex(left_i, right_i){
 
-function createLines(i, x, stage, currentLine, j){
-  if( i === n){
-    return null;
-  }
-  let color = "black";
-  if(i === currentLine){
-    color = "red";
-  }
-  if (i === j){
-    color = "blue";
-  }
-  let y_start = y_end - 25 * reverseOrder[i];
-  let line = new createjs.Shape();
-  line.graphics.setStrokeStyle(5).beginStroke(color);
-  line.graphics.lineTo(x, y_start);
-  line.graphics.lineTo(x, y_end);
-  line.graphics.endStroke();
-  stage.addChild(line);
-  createLines(i + 1, x + 25, stage, currentLine, j);
 
+  let pivot;
+  if(left_i <= right_i){
+    pivot = right_i;
+    partitionIndex = partitionByIndex(pivot, left_i, right_i);
+
+   //sort left and right
+   console.log(quickArray);
+   quickSortByIndex(left_i, partitionIndex - 1);
+   quickSortByIndex(partitionIndex + 1, right_i);
+   console.log(quickArray);
+  }
+  return quickArray;
+}
+
+function partitionByIndex(pivot, left_i, right_i){
+   var pivotValue = quickArray[pivot],
+       partitionIndex = left_i;
+   console.log(quickArray);
+
+   for(var i = left_i; i <= right_i; i++){
+     console.log(quickArray);
+    if(quickArray[i] < pivotValue){
+      swap(i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(right_i, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(i, j){
+   var temp = quickArray[i];
+   quickArray[i] = quickArray[j];
+   quickArray[j] = temp;
 }
