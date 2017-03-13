19395768c829ae299906e1e7f722056d645ca9c7
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-03-13 18:17:34 -0400

    refactor

--- initialize.js
+++ initialize.js
@@ -1,18 +1,18 @@
-let length = 10;
+// let length = 10;
 let ARRAY = [];
-document.getElementById("userLength").onchange = (e) => {
-  let new_l = parseInt(e.currentTarget.value);
-  if (new_l > 2 && new_l <= 20){
-    length = new_l
-  };
-  ARRAY = Array.apply(null, Array(length)).map(function (_, i) {return i + 1;});
- }
-let radios = document.querySelectorAll("input[type=radio]");
-for(let i = 0; i < radios.length; i++){
-  radios[i].onclick = function(){
-    init(this.value);
-  }
-}
+// document.getElementById("userLength").onchange = (e) => {
+//   let new_l = parseInt(e.currentTarget.value);
+//   if (new_l > 2 && new_l <= 20){
+//     length = new_l
+//   };
+//   ARRAY = Array.apply(null, Array(length)).map(function (_, i) {return i + 1;});
+//  }
+// let radios = document.querySelectorAll("input[type=radio]");
+// for(let i = 0; i < radios.length; i++){
+//   radios[i].onclick = function(){
+//     init(parseInt(this.value));
+//   }
+// }
 function init(arrayType) {
   if(arrayType === 1){
       ARRAY.reverse();
@@ -26,4 +26,10 @@ function init(arrayType) {
     }
     ARRAY = array;
   }
+  quickArray = ARRAY.slice();
+  quickLength = quickArray.length - 1;
+  mergeArray = ARRAY.slice();
+  bubbleArray = ARRAY.slice();
+  m = bubbleArray.length;
+  n = m + 1;
 }
--- run.js
+++ run.js
@@ -7,12 +7,18 @@
       createjs.Ticker.addEventListener("tick", stage2);
       const stage3 = new createjs.Stage("canvas3");
       createjs.Ticker.addEventListener("tick", stage3);
-      quickArray = ARRAY.slice();
-      quickLength = quickArray.length - 1;
-      mergeArray = ARRAY.slice();
-      bubbleArray = ARRAY.slice();
-      m = bubbleArray.length;
-      n = m + 1;
+      let length = document.getElementById("userLength").value > 2;
+      if (length < 2 || new_l > 20){
+        length = 10;
+      };
+      let arrayType;
+      ARRAY = Array.apply(null, Array(length)).map(function (_, i) {return i + 1;});
+      document.querySelectorAll("input[type=radio]").forEach((el) => {
+        if(el.checked){
+          arrayType = parseInt(el.value);
+        }
+      });
+      init(arrayType);
       mergeSort();
       quickSort();
       render(stage1, stage2, stage3);
